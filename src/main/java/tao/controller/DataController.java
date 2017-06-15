6e7415b3af513eb788c1b8a80ee7867eb62265ad
tonyjiangtb <tonyjiangtb@gmail.com>
2017-06-15 22:48:45 +0800

    Create DataController.java

--- src/main/java/tao/controller/DataController.java
+++ src/main/java/tao/controller/DataController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tao.service.serviceA;
 import tao.service.serviceReq;
-
+//mfk
 @RestController
 public class DataController {
 	
